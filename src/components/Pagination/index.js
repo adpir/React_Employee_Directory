61d57fa2b98d80a1b91114c60c38e29d23b8e0c6
Alice Piar <adpir30@gmail.com>
2021-03-18 02:29:38 -0500

    new pagination added

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+function Pagination() {
+    <section>
+        <nav aria-label="Page navigation example">
+            <ul className="pagination justify-content-end">
+                <li className="page-item disabled">
+                    <a className="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                </li>
+                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                <li className="page-item"><a className="page-link" href="#">2</a></li>
+                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                <li className="page-item">
+                    <a className="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+
+
+
+
+}
+
+export default Pagination;
