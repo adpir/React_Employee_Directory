d0f66e3ca6e9c62d3aa6ce0f492ab381eb11cd02
Alice Piar <adpir30@gmail.com>
2021-03-18 02:25:45 -0500

    adding new updates and deleting extra files

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,11 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./navbar.css";
-function Navbar (){
-    return(
-        <nav className ="navbar">
-       
 
-        
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand">Navbar</a>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </nav>
     )
 }
--- src/components/datatable/EmployeesDB.js
+++ src/components/datatable/EmployeesDB.js
@@ -1,45 +1,66 @@
+/* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 
 class EmployeesDB extends React.Component {
   state = {
-    employeeRecords: [],
+    loading: true,
+    people: []
   };
 
-  componentDidMount = () => {
-    API.search().then((records) => {
-      let data = records.data.results;
-      console.log(data);
-      let employeedata = [];
-      for (let i = 0; i < data.length; i++) {
-        employeedata.push({
-          firstname: data[i].name.first,
-          lastname: data[i].name.last,
-          phone: data[i].phone,
-          email: data[i].email,
-          gender: data[i].gender,
-          age: data[i].dob.age,
-          picture: data[i].picture.thumbnail,
-        });
+  async componentDidMount() {
+    const url = ('https://api.randomuser.me/?results=50')
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ people: data.results, loading: false });
+  }
 
-        this.setState({ employeeRecords: employeedata });
-      }
-    });
-  };
- 
   render() {
-    return (
-      <div className="EmployeeDB">
-        {/* <h1 className="text-center bg-danger text-center">Employee Data</h1> */}
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
 
-        <Datatable
-          rows={this.state.employeeRecords}
-        />
-       
-      </div>
-    
-    );
+    if (!this.state.people.length) {
+      return <div>didn't get a person</div>;
+    }
+
+    return (
+      <div>
+        <table className="table table-dark table-stripped">
+          <thead className="thead-font">
+            <tr>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Picture</th>
+            </tr>
+          </thead>
+          <tbody className="tbody-font">
+            {this.state.people.map(person => (
+              <tr>
+                <th>{person.name.first}</th>
+                <th scope="row">
+                  {person.name.last}</th>
+                <td>{person.gender}</td>
+                <td>{person.email}</td>
+                <td>{person.phone}</td>
+                {<td><img src={person.picture.medium} /></td>}
+              </tr>
+            ))}
+          </tbody>
+        </table >
+      </div >
+    )
+
+
+
   }
 }
 
+
+
 export default EmployeesDB;
+
+
